92dd0f770f71becf7acc1ccb67da9bcf3af21a98
DESKTOP-183T0AU\now17 <now17@nate.com>
2016-10-09 05:24:06 +0900

    no message

--- application/config/ftp_user.php
+++
@@ -1,5 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$config['hostname'] = 'localhost';
-$config['username'] = 'kunetworks';
-$config['password'] = 'revol3939';
-?>
